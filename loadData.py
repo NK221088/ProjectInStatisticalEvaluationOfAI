c20129f5a1cae27249111e7ed6a46355ca63b971
Nikolai Treschow Kühl <s221088@dtu.dk>
2025-06-10 15:49:15 +0200

    Updated the loaddata script to include all economies

--- loadData.py
+++ loadData.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from loadGroups import low
+from loadGroups import low, low_mid, upp_mid, high, South_Asia, Europe_and_Central_Asia, Middle_East_and_North_Africa, Sub_Saharan_Africa, Latin_America_and_Caribbean, East_Asia_and_Pacific, North_America, educational_low, educational_med, educational_high
 
 def load_data(filename = "un_member_states_income.csv"):
     """Load the CSV file with semicolon delimiter"""
@@ -8,7 +8,12 @@ def load_data(filename = "un_member_states_income.csv"):
 allData = load_data("answer_Data.csv")
 
 allData_lowIncome = allData[allData["country"].isin(low["Economy"])]
+allData_lowMiddleIncome = allData[allData["country"].isin(low_mid["Economy"])]
+allData_uppMiddleIncome = allData[allData["country"].isin(upp_mid["Economy"])]
+allData_highIncome = allData[allData["country"].isin(high["Economy"])]
 
-(allData_lowIncome.describe())
 
-# print(allData[allData["country"]])
+allData_lowIncome = allData_lowIncome.describe()
+allData_lowMiddleIncome = allData_lowMiddleIncome.describe()
+allData_uppMiddleIncome = allData_uppMiddleIncome.describe()
+allData_highIncome = allData_highIncome.describe()
