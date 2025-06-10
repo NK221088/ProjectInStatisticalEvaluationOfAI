3417ded77ea9ee04d6d4bfa5f8bb0aee2c556a6f
Nikolai Treschow Kühl <s221088@dtu.dk>
2025-06-10 14:39:19 +0200

    Sorted the groups we need for the analysis from the different CSV files

--- test.py
+++ test.py
@@ -1,6 +1,32 @@
 import pandas as pd
 
 # Load the CSV files
-educations = pd.read_csv('filtered_education_data.csv')
+def load_data(filename = "un_member_states_income.csv"):
+    """Load the CSV file with semicolon delimiter"""
+    return pd.read_csv(filename, delimiter=";")
 
-print(educations.describe())
+income_df = load_data()
+low = income_df[income_df["Income group"] == "Low income"]
+low_mid = income_df[income_df["Income group"] == "Lower middle income"]
+upp_mid = income_df[income_df["Income group"] == "Upper middle income"]
+high = income_df[income_df["Income group"] == "High income"]
+
+South_Asia = income_df[income_df["Region"] == "South Asia"]
+Europe_and_Central_Asia = income_df[income_df["Region"] == "Europe & Central Asia"]
+Middle_East_and_North_Africa = income_df[income_df["Region"] == "Middle East & North Africa"]
+Sub_Saharan_Africa = income_df[income_df["Region"] == "Sub-Saharan Africa"]
+Latin_America_and_Caribbean = income_df[income_df["Region"] == "Latin America & Caribbean"]
+East_Asia_and_Pacific = income_df[income_df["Region"] == "East Asia & Pacific"]
+North_America = income_df[income_df["Region"] == "North America"]
+
+education_df = load_data("filtered_education_data.csv")
+
+col_name = 'Economy,Year,Economy Code,"Educational attainment, at least completed primary, population 25+ years, total (%) (cumulative)"'
+
+# Split by comma and take the last part, then convert to numeric
+numeric_values = pd.to_numeric(education_df[col_name].str.split(',').str[-1], errors='coerce')
+
+# Filter values
+educational_low = education_df[numeric_values < 50]
+educational_med = education_df[(numeric_values >= 50) & (numeric_values < 90)]
+educational_high = education_df[numeric_values > 90]
